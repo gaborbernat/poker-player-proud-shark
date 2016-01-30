22c0ba719177ff93beac02fabc5ac612615989d0
Gabor Bernat <gabor.bernat@gravityrd.com>
2016-01-30 16:37:31 +0100

    fix that shit

--- src/test/java/org/leanpoker/player/rankers/FullRankerTest.java
+++ src/test/java/org/leanpoker/player/rankers/FullRankerTest.java
@@ -1,27 +1,28 @@
 package org.leanpoker.player.rankers;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import org.junit.Assert;
 import org.junit.Test;
-import org.leanpoker.player.Parser.Suit;
+
 import org.leanpoker.player.domain.Card;
+import org.leanpoker.player.domain.Suit;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class FullRankerTest {
 
 	@Test
-	public void testFullRanker(){
+	public void testFullRanker() {
 		List<Card> cards = new ArrayList<Card>();
 		cards.add(new Card("A", Suit.clubs.name()));
 		cards.add(new Card("A", Suit.diamonds.name()));
 		cards.add(new Card("10", Suit.clubs.name()));
 		cards.add(new Card("10", Suit.diamonds.name()));
 		cards.add(new Card("10", Suit.hearts.name()));
-		//when
+		// when
 		FullRanker underTest = new FullRanker();
 		int actual = underTest.getRank(cards);
-		//then
+		// then
 		Assert.assertEquals(558, actual);
 	}
 }
