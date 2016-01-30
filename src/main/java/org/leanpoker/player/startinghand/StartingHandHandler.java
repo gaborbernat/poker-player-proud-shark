a9e23960e0f6d01baa334684c12fd9ed80b11788
Kornel Simon <kokog.private@gmail.com>
2016-01-30 11:00:12 +0100

    something

---
+++ src/main/java/org/leanpoker/player/startinghand/StartingHandHandler.java
@@ -0,0 +1,11 @@
+package org.leanpoker.player.startinghand;
+
+import org.leanpoker.player.Parser.Card;
+
+public class StartingHandHandler {
+
+	public int getStartingHandValue(Card card1, Card card2) {
+		return 10;
+	}
+	
+}
