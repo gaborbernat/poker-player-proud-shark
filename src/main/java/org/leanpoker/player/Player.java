804baed3938dc6dea1765f58f60837ade087cb21
Gabor Bernat <gabor.bernat@gravityrd.com>
2016-01-30 11:09:43 +0100

    simple logic

--- src/main/java/org/leanpoker/player/Player.java
+++ src/main/java/org/leanpoker/player/Player.java
@@ -1,13 +1,34 @@
 package org.leanpoker.player;
 
+import org.leanpoker.player.startinghand.StartingHandHandler;
+
 import com.google.gson.JsonElement;
+
+import java.util.List;
 
 public class Player {
 
-	static final String VERSION = "Default Java folding player";
+	static final String VERSION = "Let's do this";
 
 	public static int betRequest(JsonElement request) {
-		return 0;
+		try {
+			return shouldWeGo(request) ? allInValue() : 0;
+		} catch (Exception e) {
+			System.err.println(e);
+			return 0;
+		}
+
+	}
+
+	public static boolean shouldWeGo(JsonElement element) {
+		List<Parser.Card> ourCards = Parser.getOurHand(element);
+		Parser.Card f = ourCards.get(0);
+		Parser.Card s = ourCards.get(1);
+		return new StartingHandHandler().getStartingHandValue(f, s) < 2;
+	}
+
+	private static int allInValue() {
+		return 1000000;
 	}
 
 	public static void showdown(JsonElement game) {}
