1e130a7a12d0413aff76d50ad31fc39b9db3f729
Mathe E. Botond <mathe.e.botond@gmail.com>
2016-01-30 14:40:13 +0100

    Ranker interface

---
+++ src/main/java/org/leanpoker/player/rankers/NumberRanker.java
@@ -0,0 +1,55 @@
+package org.leanpoker.player.rankers;
+
+import org.leanpoker.player.domain.Card;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Created by dell on 1/30/2016.
+ */
+public class NumberRanker implements RankerType {
+    @Override
+    public int getRank(List<Card> cards) {
+        Map<Integer, List<Card>> groups = new HashMap<>();
+
+        for (int i = 2; i <= 14; ++i) {
+            groups.put(i, new ArrayList<Card>());
+        }
+
+        for (Card card : cards) {
+            groups.get(card.getNumber()).add(card);
+        }
+
+        int max = 0, index = 0;
+        for (int i = 14; i >= 2; --i) {
+            if (groups.get(i).size() > max) {
+                max = groups.get(i).size();
+                index = i;
+            }
+        }
+
+        int rank = 0;
+        switch (max) {
+            case 2:
+                rank = 100;
+                break;
+            case 3:
+                rank = 300;
+                break;
+            case 4:
+                rank = 600;
+                break;
+        }
+
+        List bestgroup = groups.get(index);
+
+        for (Card card : groups.get(index)){
+            rank += card.getNumber();
+        }
+
+        return rank;
+    }
+}
